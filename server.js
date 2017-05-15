e63597de9eb26b7dfefbc53293f75eb080787913
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-05-15 15:02:35 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Kayvon Rahimi on 15-5-2017.
+ */
+var http = require('http');
+var express = require('express');
+var config = require('.config.json');
+
+var app = express();
+
+
+
+
+
+
+
+
+module.exports = app;
